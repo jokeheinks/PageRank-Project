5764b4da68969ac3ac7e23a9d483897fef486173
Joke Heinks <bhei@itu.dk>
2022-11-02 21:18:59 +0100

    First version of the randomSurfer algorithm

--- pagerank.py
+++ pagerank.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import random
 
 DATA_LOCATION = "PageRankExampleData/"
 
@@ -14,8 +15,40 @@ def loadData(filename):
     return G
 
 
-def randomSurfer(graph):
-    return
+def isDangling(graph, node):
+    return len(list(graph.successors(node))) == 0
+
+
+def randomNode(graph):
+    return random.choice(list(graph.nodes()))
+
+
+def addCount(graph, node):
+    graph.nodes[node]["count"] += 1
+
+
+def initCountAttr(graph):
+    for node in graph.nodes():
+        graph.nodes[node]["count"] = 0
+
+    return graph
+
+
+def randomSurfer(graph: nx.DiGraph(), n, m):
+    graph = initCountAttr(graph)
+
+    node = randomNode(graph)
+
+    for _ in range(n):
+        if random.random() > m:
+            if isDangling(graph, node):
+                node = randomNode(graph)
+            else:
+                node = random.choice(list(graph.successors(node)))
+        else:
+            node = randomNode(graph)
+        addCount(graph, node)
+    return graph
 
 
 def pageRank(graph):
@@ -31,7 +64,16 @@ def printToCSV():
 
 
 def main():
-    pass
+    graph = loadData(FILENAMES[4])
+    for node in graph.nodes():
+        print(len(list(graph.successors(node))))
+
+    graph = randomSurfer(graph, 10000, 0.15)
+
+    attrs = nx.get_node_attributes(graph, "count")
+    print(attrs)
+
+    print(nx.pagerank(graph))
 
 
 if __name__ == "__main__":
